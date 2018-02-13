4bf80788bdb342dd13177731cd616156b0e137ca
johannhawk <johannsi223@nemi.tskoli.is>
2018-02-13 04:35:59 +0000

    Add files via upload

--- v2/bookshelf/controllers.py
+++ v2/bookshelf/controllers.py
@@ -1,6 +1,29 @@
 from bookshelf import app
+from bookshelf import models as model
 from flask import render_template
 
+"""
+ Router and Controller - code that runs the model and renders the view
+"""
+# top level - index of all books (book list)
 @app.route('/')
-def index(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/index')
+def index():
+    booklist = model.booklist   # Þarf að útfæra betur. Gagnavinnsla og hjúpun á að vera í model, nota getbooklist aðferð frekar.
+    yearcount = model.yearcount
+    comp = model.comp
+    # View - the template to be completed / look and feel of the page
+    return render_template('bokalisti.html', comp=comp, booklist=booklist, yearcount=yearcount)
+
+
+# next level - isbn letter code is a specific book
+# The International Standard Book Number (ISBN) is a unique  numeric commercial book identifier.
+# https://en.wikipedia.org/wiki/International_Standard_Book_Number
+@app.route("/book/<isbn>")
+def book(isbn=None):
+    # this is what "matched" will look like
+    # matched = {"isbn":"0-943396-04-2","name":"Lord of the rings", "publisher":"KT Publishing"}
+    # matched = model.book.getbook(isbn)
+    # Samanburður (filter, regex osfrv.) á að vera í model.
+    matched = isbn # Þarf að útfæra betur
+    return render_template('book.html', matched=matched)
--- v2/bookshelf/models.py
+++ v2/bookshelf/models.py
@@ -1,1 +1,28 @@
-#class book
+# -*- coding: utf-8 -*-
+
+# Model - the business logic of the application
+
+# The model is all to do with the data that's to be handled by the application
+# and nothing to do with how it's presented / wired into the view
+# functions that work with data (files, database, api etc.)
+
+# very simple class
+class Book:
+
+    def __init__(self, name):
+        self.name = name
+
+# data
+booklist = [
+    Book("Fool's Errand"), Book("Data and goliath"), Book("Mistur")
+]
+
+yearcount = [
+    Book("2014"), Book("2016"), Book("2017")
+]
+
+comp = [
+    Book("fyr1"), Book("fyr2"), Book("fyr3")
+]
+
+# Ath þarf að útfæra betur.
